1e47172355de4f45c1c8f5ad5247b2c1a6de06d5
aidalianaf <aidalianafebiansyah@gmail.com>
2021-10-08 22:29:49 +0700

    authentication

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
